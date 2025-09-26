f8154390d17a54367b890b3f34988d48b31d965a
Varun Nema <nemavarun5@gmail.com>
2025-09-26 17:43:04 +0530

    first commit

--- tests/testerbud_invalidCred.spec.ts
+++ tests/testerbud_invalidCred.spec.ts
@@ -1,5 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://testerbud.com/');
+  await expect(page.getByRole('heading', { name: 'TesterBud – The Ultimate' })).toBeVisible();
+  await page.getByRole('button', { name: 'Practice Sites' }).click();
+  await expect(page.getByRole('link', { name: '🔐 Login Automation' })).toBeVisible();
+  await page.getByRole('link', { name: '🔐 Login Automation' }).click();
+  await expect(page.getByRole('heading', { name: 'Login to Your Practice Account' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Email Address' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('varun@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Pass123');
+  await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.locator('#root')).toContainText('Invalid email id and password');
 });
